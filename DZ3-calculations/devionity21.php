24d7dcb60f29228cb5a58011fc01cc27d0c02a3d
Tatiana Z <t.zatykyan@gmail.com>
2016-12-13 14:15:34 +0200

    rest of homework #3

---
+++ DZ3-calculations/devionity21.php
@@ -0,0 +1,10 @@
+<?php
+
+
+define ("GE", "Germany");
+
+define ("GB", "Great Britian");
+const UA = "Ukraine";
+
+$array=array(GE, GB, UA);
+print_r ($array);
---
+++ DZ3-calculations/devionity23.php
@@ -0,0 +1,9 @@
+<?php
+
+$hello='hello1';
+$hello1='hello2';
+$hello2='hello3';
+$hello3='hello4';
+$hello4='hello5';
+
+echo $$$$$hello;
